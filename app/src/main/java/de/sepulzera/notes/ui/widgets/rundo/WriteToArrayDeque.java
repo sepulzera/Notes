20ad934d2ac21c4459fbec155ccfe38e6aa4f40b
sepulzera <hartung_spam@gmx.de>
2020-01-26 09:18:48 +0100

    Add RunDo

---
+++ app/src/main/java/de/sepulzera/notes/ui/widgets/rundo/WriteToArrayDeque.java
@@ -0,0 +1,12 @@
+package de.sepulzera.notes.ui.widgets.rundo;
+
+public interface WriteToArrayDeque {
+
+    String getNewString();
+    String getOldString();
+
+    void notifyArrayDequeDataReady(SubtractStrings.Item item);
+
+    void setIsRunning(boolean isRunning);
+
+}
